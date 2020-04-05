4e44f75044b3bdf4ce96127e021acf8a595ce36d
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-06 03:50:43 +0530

    Update TestInstrument.java

--- Misc/Java/Abstract Classes/TestInstrument.java
+++ Misc/Java/Abstract Classes/TestInstrument.java
@@ -1,24 +1,17 @@
-import java.util.EventListener;
-import java.util.Random;
-
 public class TestInstrument{
     public static void main(String[] args) {
         Instrument inst[] = new Instrument[10];
-        Random rand = new Random();
         for (int i = 0; i < inst.length; i++){
-            int randNum = rand.nextInt(3) + 1;
-            switch (randNum){
-                case 1:
-                    inst[i] = new Piano();
-                    break;
-                case 2:
-                    inst[i] = new Flute();
-                    break;
-                case 3:
-                    inst[i] = new Guitar();
-                    break;
-            }
+
+            if (i == 1 || i == 4 || i == 7 )
+                inst[i] = new Piano();
+            else if (i == 2 || i == 6 || i == 8)
+                inst[i] = new Flute();
+            else
+                inst[i] = new Guitar();
+
             inst[i].play();
+
             if (inst[i] instanceof Piano)
                 System.out.println("> Instance of Piano");
             else if (inst[i] instanceof Flute)
