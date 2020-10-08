d9113f30165ed33d4c5d8f4bba32f5768844ba24
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-08 20:22:18 +0530

    update CheckKeyword.py

--- Misc/CheckKeyword.py
+++ Misc/CheckKeyword.py
@@ -1,5 +1,6 @@
 # Solution for TCS-NQT 2020 exams code section, written on 03/08/19
-# The problem was to check whether a given input is keyword or not in a certain programming language.
+# The problem was to check whether a given input is keyword or not in a certain programming language
+
 import keyword as kw
 word = input()
 if word in kw.kwlist:
